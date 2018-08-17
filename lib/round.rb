a16e36ad8d3a98982b669e51c09e33560f1ac773
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-17 13:21:04 -0600

    completes iteration 2

---
+++ lib/round.rb
@@ -0,0 +1,27 @@
+class Round
+  attr_reader :deck, :guesses, :number_correct
+  def initialize(deck)
+    @deck = deck
+    @guesses = []
+    @number_correct = 0
+  end
+
+  def current_card
+    deck.cards[0]
+  end
+
+  def record_guess(response)
+    card_attributes = "#{response[:value]} of #{response[:suit]}"
+     guess = Guess.new(card_attributes, current_card)
+     @guesses << guess
+     if guess.feedback == "Correct!"
+       @number_correct += 1
+     end
+     deck.cards.shift
+     guess
+  end
+
+  def percent_correct
+    @number_correct.to_f/@guesses.count * 100
+  end
+end
