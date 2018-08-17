657cd77b9798dc948ad23b5657b0f07e46c4d098
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-17 13:26:22 -0600

    writes test for sort

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -20,4 +20,15 @@ class DeckTest < Minitest::Test
     deck = Deck.new([card_1, card_2, card_3])
     assert_equal 3, deck.count
   end
+
+  def test_deck_can_sort_cards
+    card_1 = Card.new("4","Hearts")
+    card_2 = Card.new("Jack", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    card_4 = Card.new("Ace", "Spades")
+    card_5 = Card.new("Ace", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3, card_4, card_5])
+
+    assert_equal [card_1, card_3, card_2, card_5, card_4], deck.sort
+  end
 end
