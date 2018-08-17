9ae154cd33eb907d4a2824bec22603644312f317
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-17 11:13:24 -0600

    creates card class with value and suit

---
+++ test/card_test.rb
@@ -0,0 +1,21 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/card.rb'
+
+class CardTest < Minitest::Test
+  def test_card_exists
+    card = Card.new("Ace", "Spades")
+    assert_instance_of Card, card
+  end
+
+  def test_card_has_a_value
+    card = Card.new("Ace", "Spades")
+    assert_equal "Ace", card.value
+  end
+
+  def test_card_has_a_suit
+    card = Card.new("Ace", "Spades")
+    assert_equal "Spades", card.suit
+  end
+
+end
