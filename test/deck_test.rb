66ce606983c7cc86fccdbf737013af9ef6dd6e38
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-08-17 12:02:01 -0600

    completes deck class and test for iteration 2

---
+++ test/deck_test.rb
@@ -0,0 +1,23 @@
+require 'Minitest/autorun'
+require 'Minitest/pride'
+require './lib/card.rb'
+require './lib/guess.rb'
+require './lib/deck.rb'
+
+class DeckTest < Minitest::Test
+  def test_deck_has_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal [card_1, card_2, card_3], deck.cards
+  end
+
+  def test_deck_can_count_cards
+    card_1 = Card.new("3","Hearts")
+    card_2 = Card.new("4", "Clubs")
+    card_3 = Card.new("5", "Diamonds")
+    deck = Deck.new([card_1, card_2, card_3])
+    assert_equal 3, deck.count
+  end
+end
